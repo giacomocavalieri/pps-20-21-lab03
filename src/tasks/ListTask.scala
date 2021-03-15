b3fe4ccc7765bec6f9c0bd84cf7b2ab3f5b95b78
Giacomo Cavalieri <giacomo.cavalieri2@studio.unibo.it>
2021-03-15 11:54:07 +0100

    Map test and implementation

--- src/tasks/ListTask.scala
+++ src/tasks/ListTask.scala
@@ -6,15 +6,17 @@ import u03.Lists.List._
 import scala.annotation.tailrec
 
 object ListTask {
-    @tailrec
-    def drop[A](l: List[A], n: Int): List[A] = (l, n) match {
-      case (list, 0) => list
-      case (Nil(), _) => Nil()
-      case (Cons(_, tail), n) => drop(tail, n-1)
-    }
+  @tailrec
+  def drop[A](l: List[A], n: Int): List[A] = (l, n) match {
+    case (list, 0) => list
+    case (Nil(), _) => Nil()
+    case (Cons(_, tail), n) => drop(tail, n-1)
+  }
 
-    def flatMap[A, B](l: List[A])(mapper: A => List[B]): List[B] = l match {
-      case Cons(head, tail) => append(mapper(head), flatMap(tail)(mapper))
-      case Nil() => Nil()
-    }
+  def flatMap[A, B](l: List[A])(mapper: A => List[B]): List[B] = l match {
+    case Cons(head, tail) => append(mapper(head), flatMap(tail)(mapper))
+    case Nil() => Nil()
+  }
+
+  def map[A, B](l: List[A])(mapper: A => B): List[B] = flatMap(l)(a => List.of(mapper(a)))
 }
--- test/tasks/TestLists.scala
+++ test/tasks/TestLists.scala
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import tasks.ListTask._
 import u03.Lists._
-import u03.Lists.List._
+import u03.Lists.List.append
 
 class TestLists {
   @Test def testDrop(): Unit = {
@@ -19,5 +19,11 @@ class TestLists {
     val list = List.of(10, 20)
     assertEquals(List.of(20, 40), flatMap(list)((x: Int) => List.of(2 * x)))
     assertEquals(append(List.of(10, 10), List.of(20, 20)), flatMap(list)(x => List.of(x, x)))
+  }
+
+  @Test def testMap(): Unit = {
+    val list = List.of(10, 20)
+    assertEquals(List.of(20, 40), map(list)(_ * 2))
+    assertEquals(List.of(true, false), map(list)(_ < 15))
   }
 }
