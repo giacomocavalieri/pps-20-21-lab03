a9fe87e37d05a22781d2e37a2dacd74668a8b4a1
Giacomo Cavalieri <giacomo.cavalieri2@studio.unibo.it>
2021-03-15 12:04:17 +0100

    max test

--- test/tasks/TestLists.scala
+++ test/tasks/TestLists.scala
@@ -5,33 +5,36 @@ import org.junit.jupiter.api.Test
 import tasks.ListTask._
 import u03.Lists._
 import u03.Lists.List.append
+import u02.Optionals._
 
 class TestLists {
+  val list = List.of(10, 20)
+
   @Test def testDrop(): Unit = {
-    val list = List.of(10, 20, 30)
-    assertEquals(List.of(20, 30), drop(list, 1))
-    assertEquals(List.of(30), drop(list, 2))
+    assertEquals(List.of(20), drop(list, 1))
+    assertEquals(List.Nil(), drop(list, 2))
     assertEquals(List.Nil(), drop(list, 3))
-    assertEquals(List.Nil(), drop(list, 4))
   }
 
   @Test def testFlatMap(): Unit = {
-    val list = List.of(10, 20)
     assertEquals(List.of(20, 40), flatMap(list)((x: Int) => List.of(2 * x)))
     assertEquals(append(List.of(10, 10), List.of(20, 20)), flatMap(list)(x => List.of(x, x)))
   }
 
   @Test def testMap(): Unit = {
-    val list = List.of(10, 20)
     assertEquals(List.of(20, 40), map(list)(_ * 2))
     assertEquals(List.of(true, false), map(list)(_ < 15))
   }
 
   @Test def testFilter(): Unit = {
-    val list = List.of(10, 20)
     assertEquals(List.Nil(), filter(list)(_ => false))
     assertEquals(list, filter(list)(_ => true))
     assertEquals(List.of(10), filter(list)(_ < 15))
     assertEquals(List.of(20), filter(list)(_ > 15))
+  }
+
+  @Test def testMax(): Unit = {
+    assertEquals(Option.Some(20), max(list))
+    assertEquals(Option.None(), max(List.Nil()))
   }
 }
