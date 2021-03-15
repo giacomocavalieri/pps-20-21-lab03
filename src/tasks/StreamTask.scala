9bf09541c48cae11941629e2c32a5ef69974b878
Giacomo Cavalieri <giacomo.cavalieri2@studio.unibo.it>
2021-03-15 14:16:44 +0100

    drop implementation

--- src/tasks/StreamTask.scala
+++ src/tasks/StreamTask.scala
@@ -1,5 +1,12 @@
 package tasks
 
-object StreamTask {
+import u03.Streams.Stream
+import u03.Streams.Stream._
 
+object StreamTask {
+  def drop[A](stream: Stream[A])(n: Int): Stream[A] = (stream, n) match {
+    case (stream, 0) => stream
+    case (Empty(), n) => empty()
+    case (Cons(fh, ft), n) => drop(ft())(n-1)
+  }
 }
--- test/tasks/TestStream.scala
+++ test/tasks/TestStream.scala
@@ -5,15 +5,16 @@ import org.junit.jupiter.api.{BeforeEach, Test}
 import u03.Lists._
 import u03.Streams.Stream
 import u03.Streams.Stream._
+import tasks.StreamTask._
 
 class TestStream {
   val testStream: Stream[Int] = take(iterate(1)(_ + 1))(3)
 
   @Test def testDrop(): Unit = {
-    assertStreamEqualsList(List.of(2, 3), drop(1, testStream))
-    assertStreamEqualsList(List.of(3), drop(2, testStream))
-    assertStreamEqualsList(List.Nil(), drop(3, testStream))
-    assertStreamEqualsList(List.Nil(), drop(4, testStream))
+    assertStreamEqualsList(List.of(2, 3), drop(testStream)(1))
+    assertStreamEqualsList(List.of(3), drop(testStream)(2))
+    assertStreamEqualsList(List.Nil(), drop(testStream)(3))
+    assertStreamEqualsList(List.Nil(), drop(testStream)(4))
   }
 
   def assertStreamEqualsList[A](expected: List[A], actual: Stream[A]): Unit = {
