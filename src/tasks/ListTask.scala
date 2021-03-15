434fe2ccc2a49c8c4331a935655dd66bd4fa1a58
Giacomo Cavalieri <giacomo.cavalieri2@studio.unibo.it>
2021-03-15 11:44:23 +0100

    Remove code repetition

---
+++ src/tasks/ListTask.scala
@@ -0,0 +1,20 @@
+package tasks
+
+import u03.Lists._
+import u03.Lists.List._
+
+import scala.annotation.tailrec
+
+object ListTask {
+    @tailrec
+    def drop[A](l: List[A], n: Int): List[A] = (l, n) match {
+      case (list, 0) => list
+      case (Nil(), _) => Nil()
+      case (Cons(_, tail), n) => drop(tail, n-1)
+    }
+
+    def flatMap[A, B](l: List[A])(mapper: A => List[B]): List[B] = l match {
+      case Cons(head, tail) => ???
+      case Nil() => Nil()
+    }
+}
--- test/tasks/TestLists.scala
+++ test/tasks/TestLists.scala
@@ -2,16 +2,17 @@ package tasks
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import tasks.Lists._
-import tasks.Lists.List._
+import tasks.ListTask._
+import u03.Lists._
+import u03.Lists.List._
 
 class TestLists {
   @Test def testDrop(): Unit = {
     val list = List.of(10, 20, 30)
     assertEquals(List.of(20, 30), drop(list, 1))
     assertEquals(List.of(30), drop(list, 2))
-    assertEquals(List.empty(), drop(list, 3))
-    assertEquals(List.empty(), drop(list, 4))
+    assertEquals(List.Nil(), drop(list, 3))
+    assertEquals(List.Nil(), drop(list, 4))
   }
 
   @Test def testFlatMap(): Unit = {
