3d0cbe085fa64e268a8e01a0e632860e56274888
Giacomo Cavalieri <giacomo.cavalieri2@studio.unibo.it>
2021-03-15 13:57:00 +0100

    Refactor TestLists

--- test/tasks/TestLists.scala
+++ test/tasks/TestLists.scala
@@ -9,33 +9,33 @@ import u03.Lists.List.append
 import u02.Optionals._
 
 class TestLists {
-  val list = List.of(10, 20)
+  val testList = List.of(10, 20)
 
   @Test def testDrop(): Unit = {
-    assertEquals(List.of(20), drop(list, 1))
-    assertEquals(List.Nil(), drop(list, 2))
-    assertEquals(List.Nil(), drop(list, 3))
+    assertEquals(List.of(20), drop(testList, 1))
+    assertEquals(List.Nil(), drop(testList, 2))
+    assertEquals(List.Nil(), drop(testList, 3))
   }
 
   @Test def testFlatMap(): Unit = {
-    assertEquals(List.of(20, 40), flatMap(list)((x: Int) => List.of(2 * x)))
-    assertEquals(append(List.of(10, 10), List.of(20, 20)), flatMap(list)(x => List.of(x, x)))
+    assertEquals(List.of(20, 40), flatMap(testList)((x: Int) => List.of(2 * x)))
+    assertEquals(append(List.of(10, 10), List.of(20, 20)), flatMap(testList)(x => List.of(x, x)))
   }
 
   @Test def testMap(): Unit = {
-    assertEquals(List.of(20, 40), map(list)(_ * 2))
-    assertEquals(List.of(true, false), map(list)(_ < 15))
+    assertEquals(List.of(20, 40), map(testList)(_ * 2))
+    assertEquals(List.of(true, false), map(testList)(_ < 15))
   }
 
   @Test def testFilter(): Unit = {
-    assertEquals(List.Nil(), filter(list)(_ => false))
-    assertEquals(list, filter(list)(_ => true))
-    assertEquals(List.of(10), filter(list)(_ < 15))
-    assertEquals(List.of(20), filter(list)(_ > 15))
+    assertEquals(List.Nil(), filter(testList)(_ => false))
+    assertEquals(testList, filter(testList)(_ => true))
+    assertEquals(List.of(10), filter(testList)(_ < 15))
+    assertEquals(List.of(20), filter(testList)(_ > 15))
   }
 
   @Test def testMax(): Unit = {
-    assertEquals(Option.Some(20), max(list))
+    assertEquals(Option.Some(20), max(testList))
     assertEquals(Option.None(), max(List.Nil()))
   }
 
@@ -47,13 +47,13 @@ class TestLists {
 
   @Test def testFoldLeft(): Unit = {
     assertEquals(0, foldLeft(List.Nil[Int]())(0)(_ + _))
-    assertEquals(30, foldLeft(list)(0)(_ + _))
-    assertEquals(-30, foldLeft(list)(0)(_ - _))
+    assertEquals(30, foldLeft(testList)(0)(_ + _))
+    assertEquals(-30, foldLeft(testList)(0)(_ - _))
   }
 
   @Test def testFoldRight(): Unit = {
     assertEquals(0, foldRight(List.Nil[Int]())(0)(_ + _))
-    assertEquals(30, foldRight(list)(0)(_ + _))
-    assertEquals(-10, foldRight(list)(0)(_ - _))
+    assertEquals(30, foldRight(testList)(0)(_ + _))
+    assertEquals(-10, foldRight(testList)(0)(_ - _))
   }
 }
